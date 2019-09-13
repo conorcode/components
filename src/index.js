95e143c447b65c29378b4122a3ad4f161bccbf03
argyris <Konstantinos.ARGYRIS@intrasoft-intl.com>
2019-09-13 12:41:54 +0300

    ApprovalCard added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import faker from 'faker';
-import CommentDetail from './CommentDetail'
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-        <CommentDetail />
+      <ApprovalCard inputText="my text goes here!!" />
+      <CommentDetail
+        author="Ted"
+        timeAgo="Today at 6:54pm"
+        blogText="Ted blog input"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Bill"
+        timeAgo="Yesterday at 4:34am"
+        blogText="Bill says hello"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
